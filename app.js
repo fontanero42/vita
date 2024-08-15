17db01f5e37422d6ecf8ae5e01bbc0a478860a88
fontanero42 <admin@bernd-mausehund.de>
2024-08-15 17:42:18 +0200

    consent banner

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const storageType = localStorage;
+const consentPropertyName = 'zapatero_consent';
+const coolDown = 24 * 60 * 60 * 1000;
+
 let zapatero_mode = {
   'ad_storage': 'denied',
   'analytics_storage': 'denied',
@@ -6,16 +10,43 @@ let zapatero_mode = {
   'security_storage': 'denied',
 };
 
-
-window.onload = () => {
-const acceptButton = document.querySelector("[data-modal-accept]");
-const modal = document.querySelector("[data-modal]");
-
-modal.showModal();
-
-acceptButton.addEventListener("click",()=>{
-  modal.close();
-});
+const shouldShowModal = () => {
+  const compare = new Date().getTime() - coolDown;
+  if (storageType.getItem(consentPropertyName) < compare)
+    return true;
+  else
+    return false;
 
 }
 
+const saveToStorage = () => storageType.setItem(consentPropertyName, new Date().getTime());
+
+window.onload = () => {
+  const acceptBtn = document.querySelector("[data-modal-accept]");
+  const denyBtn = document.querySelector("[data-modal-deny]");
+
+  const modal = document.querySelector("[data-modal]");
+
+  modal.showModal();
+
+  acceptBtn.addEventListener("click", () => {
+    saveToStorage(storageType);
+    modal.close();
+    zapatero_mode['analytics_storage'] = 'granted';
+    zapatero_mode['ad_storage'] = 'granted';
+    gtag('consent', 'update', zapatero_mode);
+    localStorage.setItem('consent', JSON.stringify(zapatero_mode));
+  });
+  /* acceptButton.classList.toggle("focus");*/
+
+
+  denyBtn.addEventListener('click', (event) => {
+    saveToStorage(storageType);
+    //  consentPopup.classList.add('hide');
+    modal.close();
+    zapatero_mode['analytics_storage'] = 'denied';
+    zapatero_mode['ad_storage'] = 'denied';
+    gtag('consent', 'update', zapatero_mode);
+    localStorage.setItem('consent', JSON.stringify(zapatero_mode));
+  });
+}
